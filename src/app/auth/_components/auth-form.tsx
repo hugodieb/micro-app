d28b9b6ee6ce44e6656c18a00157e7997443179c
Hugo Dieb <hugodieb.hd@gmail.com>
2024-07-22 17:01:29 -0300

    banco sqlite salvando dados

---
+++ src/app/auth/_components/auth-form.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { useForm } from 'react-hook-form'
+import { signIn } from 'next-auth/react'
+import { toast } from '@/components/ui/use-toast'
+
+export function AuthForm() {
+  const form = useForm()
+
+  const handleSubmit = form.handleSubmit(async (data) => {
+    try {
+      await signIn('email', { email: data.email })
+
+      toast({
+        title: 'Magic Link Sent',
+        description: 'Check your email for the magic link to login',
+      })
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'An error occurred. Please try again.',
+      })
+    }
+  })
+
+  return (
+    <div className="mx-auto max-w-md space-y-6">
+      <div className="space-y-2 text-center">
+        <h1 className="text-3xl font-bold">Login</h1>
+        <p className="text-sm text-muted-foreground">
+          Entre com seu endereço de E-mail.
+        </p>
+      </div>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required {...form.register('email')} />
+        </div>
+        <Button type="submit" className="w-full">
+          Send Magic Link
+        </Button>
+      </form>
+    </div>
+  )
+}
